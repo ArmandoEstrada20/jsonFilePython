26b28cd7f17c89c07a9973abc9ec866d0c2a26da
Hector Armando Ramirez Estrada <137834404+ArmandoEstrada20@users.noreply.github.com>
2024-02-17 01:11:02 +0000

    Se añadió una función que agrega datos al archivo JSON desde la consola

--- archivo.py
+++ archivo.py
@@ -16,6 +16,38 @@ def verDatos(archivo):
             print('Su paso por el fútbol: ', jugador["equipos"])
             print('')
             
+def agregarJugador(archivo):
+    # Solicita al usuario que ingrese la información del nuevo jugador
+    nombre = input("Ingresa el nombre del jugador: ")
+    apellido = input("Ingresa el apellido del jugador: ")
+    edad = int(input("Ingresa la edad del jugador: "))
+    
+    equipos = []
+    while True:
+        equipo = input("Ingresa un equipo en el que ha estado el jugador (teclea 'salir' para terminar): ")
+        if equipo.lower() == 'salir':
+            break
+        equipos.append(equipo)
+
+    nuevo_jugador = {
+        "nombre": nombre,
+        "apellido": apellido,
+        "edad": edad,
+        "equipos": equipos
+    }
+
+    # Abre el archivo JSON y carga los datos
+    with open(archivo, 'r', encoding='utf-8') as datos:
+        plantilla = json.load(datos)
+
+    # Agrega el nuevo jugador a la lista de jugadores
+    plantilla["jugadores"].append(nuevo_jugador)
+
+    # Escribe los datos actualizados en el archivo JSON
+    with open(archivo, 'w', encoding='utf-8') as datos:
+        json.dump(plantilla, datos, ensure_ascii=False, indent=4)
+
 if __name__ == '__main__':
     archivo = 'Data/data.json' # Variable que almacena la ubicación del archivo JSON
     verDatos(archivo)
+    #agregarJugador(archivo)
